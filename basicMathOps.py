db450da71a992ec5846b0e469f0de848de38c227
davidsbv <dev.perez.ruiz@gmail.com>
2024-04-12 18:24:01 +0200

    Se completan el resto de operaciones matemáticas y sus test

--- basicMathOps.py
+++ basicMathOps.py
@@ -7,3 +7,25 @@ if __name__ == "__main__":
     print(sumar(5, 3))
 
 
+def restar(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print(restar(5, 3))
+
+
+def multiplicar(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print(multiplicar(5, 3))
+
+
+def dividir(a, b):
+    try:
+        return round((a / b), 3)
+    except ZeroDivisionError:
+        print("Error. No se puede dividir entre cero")
+
+if __name__ == "__main__":
+    print(dividir(5, 3))
--- test_basicMathOps.py
+++ test_basicMathOps.py
@@ -2,16 +2,41 @@
 
 # Librería para pruebas unitarias
 import unittest
-from basicMathOps import sumar
+from basicMathOps import *
 
 # Clase que recibe una prueba unitaria como parámetro
 class TestSumar(unittest.TestCase):
 
-    # Método para probar suma
+    # Función para probar suma con tres llamadas al método assertEqual
     def test_sumar(self):
         self.assertEqual(sumar(5, 3), 8)
-        self.assertEqual(sumar(1, -1), -0)
+        self.assertEqual(sumar(1, -1), 0)
         self.assertEqual(sumar(-1, -1), -2)
+
+class TestResta(unittest.TestCase):
+
+    def test_restar(self):
+        self.assertEqual(restar(5, 3), 2)
+        self.assertEqual(restar(-5, 3), -8)
+        self.assertEqual(restar(5, -3), -8)
+        self.assertEqual(restar(-5, -3), -2)
+
+class TestMultiplicacion(unittest.TestCase):
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(5, 3), 15)
+        self.assertEqual(multiplicar(-5, 3), -15)
+        self.assertEqual(multiplicar(5, -3), -15)
+        self.assertEqual(multiplicar(-5, -3), 15)
+
+class TestDivision(unittest.TestCase):
+
+    def test_dividir(self):
+        self.assertEqual(dividir(15, 3), 5)
+        self.assertEqual(dividir(15, -3), -5)
+        self.assertEqual(dividir(-15, 3), -5)
+        with self.assertRaises(ZeroDivisionError):
+            dividir(15,0)
 
 if __name__ == '__main__':
     unittest.main()
