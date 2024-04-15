763ce8ca099fae140f6f8927c55149b39890fa7f
davidsbv <dev.perez.ruiz@gmail.com>
2024-04-15 17:31:59 +0200

    Agrego comentario a la función dividir

--- basicMathOps.py
+++ basicMathOps.py
@@ -24,8 +24,11 @@ if __name__ == "__main__":
 def dividir(a, b):
     try:
         return round((a / b))
+    
     except ZeroDivisionError:
         print("Error. No se puede dividir entre cero")
+        #Se propaga la excepción para comprobar que funciona en las pruebas unitarias
         raise
+
 if __name__ == "__main__":
     print(dividir(5, 3))
